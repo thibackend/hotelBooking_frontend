5496c5e026f937bafb9b9b77c9bc39fd498be0ec
Tien-PM24 <115676176+Tien-PM24@users.noreply.github.com>
2023-06-13 20:26:46 +0700

    first my task

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import Router from "./routes";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react';
+import DetailRoom from "./pages/detail";
 
 const App = () => {
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Router />
-        <ToastContainer />
-      </BrowserRouter>
-    </ChakraProvider>
+   
+        <DetailRoom/>
+
+
 
 
   );
