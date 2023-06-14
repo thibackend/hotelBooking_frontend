552e54d5fceff2f367f8db5298605e7bc0aa0c99
a du <du.a24@student.passerellesnumeriques.org>
2023-06-14 11:11:50 +0700

    add checkout page

---
+++ src/pages/checkout/index.jsx
@@ -0,0 +1,13 @@
+import ConfirmationPage from "./comfitBooking";
+import React from "react";
+
+
+function Checkout() {
+    return ( 
+        <>
+        <ConfirmationPage/>
+        </>
+     );
+}
+
+export default Checkout;
