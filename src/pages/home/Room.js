27808a355fd36af996d30e05a6d0e248a00140bc
Thắng Nguyễn <thang.nguyen24@student.passerellesnumeriques.org>
2023-06-19 12:09:43 +0700

    add chinh sua in ra hompage

--- src/pages/home/Room.js
+++ src/pages/home/Room.js
@@ -8,28 +8,58 @@ import {
   Flex,
 } from "@chakra-ui/react";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import { Icon } from "@chakra-ui/react";
 // import { FaStar } from "react-icons/fa";
 import "./Room.css";
 import { Grid, Box } from "@chakra-ui/react";
-
 const MyComponent = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
+  const [datahotels, setDatahotels] = useState([]) || undefined;
 
-  async function fetchData() {
-    try {
-      const response = await axios.get(
-        "https://63a571e42a73744b008e23f7.mockapi.io/API/product"
-      );
-      // Handle the response data
-      console.log(setData(response.data));
-    } catch (error) {
-      // Error handling code here
-      console.log(error);
-    }
+  if (data) {
+    let hotel;
+    data.hotels.map((e) => {
+      let a = e.id;
+      let image = '';
+      data.images.filter((id) => id.hotel_id == a)
+        .filter((id) => id.hotel_id == a)
+        .map((img) => {
+          image =img.image;
+          return null;
+        });
+
+      let datahotel = [
+        {
+        'id':a,
+        "name":e.name,
+        "address":e.address,
+        "image":image,
+        "contact":e.contact,
+        "star":e.star,
+        "status":e.status,
+        }
+      ]
+      if(!datahotels){
+        setDatahotels(datahotel);
+      }else{
+        hotel = datahotels.concat(datahotel);
+      }
+    });
+    setDatahotels(hotel);
+    console.log('===============data hotels=====================');
+    console.log(datahotels);
+    console.log('====================================');
   }
-  fetchData();
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    await axios.get("http://127.0.0.1:8000/api/hotels").then((res) => {
+      setData(res.data);
+    });
+  };
   return (
     <Grid
       templateColumns="repeat(4, 1fr)"
@@ -37,51 +67,49 @@ const MyComponent = () => {
       justifyItems="center"
       mx="auto"
     >
-      {data.map(e => (
-        <Box alignSelf="flex-start" key={e.ID}>
-          <Card maxW="sm" p="4" mt="4">
-            <CardBody className="Box">
-              <Image
-                src={e.avatar}
-                alt="Green double couch with wooden legs"
-                borderRadius="20px"
-                width="400px"
-                height="350px"
-                cursor="pointer"
-                objectFit="cover"
-              />
+      {data
+        ? data.hotels.map((e) => (
+            <Box alignSelf="flex-start" key={e.id}>
+              <Card maxW="sm" p="4" mt="4">
+                <CardBody className="Box">
+                  <Image
+                    src={""}
+                    alt="Green double couch with wooden legs"
+                    borderRadius="20px"
+                    width="400px"
+                    height="350px"
+                    cursor="pointer"
+                    objectFit="cover"
+                  />
 
-              <Stack mt="6" spacing="0">
-                <Flex alignItems="center">
-                  <a href="" size="md" className="title" ml="auto">
-                    <b>{e.name}</b>
-                  </a>
-                  {/* <Icon
+                  <Stack mt="6" spacing="0">
+                    <Flex alignItems="center">
+                      <a href="" size="md" className="title" ml="auto">
+                        <b>{e.name}</b>
+                      </a>
+                      {/* <Icon
                     as={FaStar}
                     color="yellow.500"
                     boxSize={18}
                     mr={1}
                     className="star-icon"
                   /> */}
-                  <i className="fa-sharp fa-regular fa-star"></i>
-                  <Text fontSize="sm">25 Likes</Text>
-                </Flex> 
+                      <i className="fa-sharp fa-regular fa-star"></i>{" "}
+                      <Text fontSize="sm"></Text>
+                    </Flex>
 
-                <Text className="infor">
-                  Địa điểm: Singapore <br />
-                  Ngày hoạt động : {e.date}
-                </Text>
-                <b>${e.price} / Đêm</b>
-              </Stack>
-            </CardBody>
-            <Divider />
-          </Card>
-        </Box>
-      ))}
-
-      <>
-        
-      </>
+                    <Text className="infor">
+                      Địa điểm: {e.address} <br />
+                      hoạt động : {e.status ? "Open" : "Closed"}
+                    </Text>
+                    <b>${"100 $"} / Đêm</b>
+                  </Stack>
+                </CardBody>
+                <Divider />
+              </Card>
+            </Box>
+          ))
+        : ""}
     </Grid>
   );
 };
