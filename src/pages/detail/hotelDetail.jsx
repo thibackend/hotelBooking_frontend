d08bc053fb2d8379976bb2b044a45ca0346c83f8
Tien-PM24 <115676176+Tien-PM24@users.noreply.github.com>
2023-06-27 08:53:12 +0700

    Delete some Information not have in DB

--- src/pages/detail/hotelDetail.jsx
+++ src/pages/detail/hotelDetail.jsx
@@ -64,47 +64,6 @@ function HotelDetail() {
                     <Text>Địa chỉ: {dataRun.address}</Text>
                     <Text>{dataRun.desc}</Text>
                     <Text>Liên hệ chủ nhà {dataRun.contact}</Text>
-                    <Text>12 khách 3 phòng ngủ 9 giường4 phòng tắm đầy đủ và 1 phòng vệ sinh cơ bản</Text>
-                    <Box>
-                        <Text as="b">Không gian riêng để làm việc</Text>
-                        <Text>Một căn phòng có Wi-fi, rất phù hợp để làm việc.</Text>
-                        <Text as="b">Tự nhận phòng</Text>
-                        <Text>Bạn có thể gặp nhân viên trực cửa để nhận phòng.</Text>
-                        <Text as="b">Pongkuna (Praow) là Chủ nhà siêu cấp</Text>
-                        <Text>Chủ nhà siêu cấp là những chủ nhà có kinh nghiệm, được đánh giá cao và là những người cam kết mang lại quãng thời gian ở tuyệt vời cho khách.</Text>
-                    </Box>
-                    <Heading as="h5">Nơi bạn sẽ ngủ nghỉ</Heading>
-                    <SimpleGrid columns={3} marginLeft="10px">
-                        <Box border="solid 1px gray" w="50%" p="10px" textAlign="center" borderRadius="20px">
-                            <Text as="b">Phòng ngủ 1</Text>
-                            <Text>1 giường queen</Text>
-                        </Box>
-                        <Box border="solid 1px gray" w="50%" p="10px" textAlign="center" borderRadius="20px">
-                            <Text as="b">Phòng ngủ 2</Text>
-                            <Text>1 giường đôi</Text>
-                        </Box>
-                        <Box border="solid 1px gray" w="50%" p="10px" textAlign="center" borderRadius="20px">
-                            <Text as="b">Phòng ngủ 3</Text>
-                            <Text>1 giường queen, 6 giường đơn</Text>
-                        </Box>
-                    </SimpleGrid>
-                    <Heading as="h5">Nơi này có những gì cho bạn</Heading>
-                    <SimpleGrid columns={2}>
-                        <Box>
-                            <Text>Hướng nhìn ra núi</Text>
-                            <Text>Wi-fi</Text>
-                            <Text>Chỗ đỗ xe miễn phí tại nơi ở</Text>
-                            <Text>Bồn tắm nước nóng</Text>
-                            <Text>Máy phát hiện khí CO</Text>
-                        </Box>
-                        <Box>
-                            <Text>Bếp</Text>
-                            <Text>Không gian riêng để làm việc</Text>
-                            <Text>Hồ bơi Riêng</Text>
-                            <Text>Camera an ninh trong nhà</Text>
-                            <Text>Máy báo khói</Text>
-                        </Box>
-                    </SimpleGrid>
                 </>
             ) : (
                 <p>Loading...</p>
