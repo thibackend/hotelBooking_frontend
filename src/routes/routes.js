71235f6c5261276d4721cee1e38b5434692e4753
ngocsondinh668@gmail.com <ngocsondinh668@gmail.com>
2023-06-28 08:49:54 +0700

    file and folder layout admin

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,6 +8,10 @@ import Rooms from "../pages/room";
 import HotelDetail from "../pages/detail/hotelDetail";
 import ConfirmCheckout from "../pages/checkout/ConfirmCheckout";
 import Search from "../pages/search";
+import Admin from "../pages/Admin";
+import Room_management from "../pages/Admin/room_management/room_manag";
+import Booking_mang from "../pages/Admin/booking_management/booking_ma";
+import User_manag from "../pages/Admin/User_management/user_manag";
 export const routes = () => [
   {
     path: "/auth",
@@ -27,6 +31,16 @@ export const routes = () => [
       { path: "detailHotel/:id", element: <HotelDetail /> },
       { path: "confirm", element: <ConfirmCheckout /> },
       { path: "search", element: <Search /> },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <Admin/>,
+    children: [
+      { path: "", element: <Room_management/> },
+      { path: "user_manag", element: <User_manag/> },
+      { path: "room_manag", element: <Room_management/> },
+      { path: "booking_manag", element: <Booking_mang/> },
     ],
   },
   {
