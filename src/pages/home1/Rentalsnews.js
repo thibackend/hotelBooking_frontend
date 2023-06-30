15d591816f320231b46b3046fee5ebda1b103775
ngocsondinh668@gmail.com <ngocsondinh668@gmail.com>
2023-06-30 16:45:42 +0700

    filter

--- src/pages/home1/Rentalsnews.js
+++ src/pages/home1/Rentalsnews.js
@@ -8,15 +8,9 @@ import { FiSearch } from "react-icons/fi";
 const Rentals = (props) => {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
-  const [showFilter, setShowFilter] = useState(false);
   const [selectedOptions, setSelectedOptions] = useState("");
   const [roomPrice, setRoomPrice] = useState(null);
-  const [services, setServices] = useState([]);
-  const [roomServices, setRoomServices] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [selectedServices, setSelectedServices] = useState([]);
-
-  const filterRef = useRef(null);
 
   const handleOptionChange = (event) => {
     const selectedValue = event.target.value;
@@ -28,63 +22,12 @@ const Rentals = (props) => {
     setRoomPrice(newPrice || 0);
   };
 
-  const handleIconClick = () => {
-    setShowFilter(!showFilter);
-  };
-
   const handleAllOption = () => {
     setSelectedOptions("");
     setRoomPrice(null);
-    setSelectedServices([]);
-  };
-
-  const handleServiceChange = (event) => {
-    const serviceId = event.target.value;
-    setSelectedServices((prevServices) => {
-      if (prevServices.includes(serviceId)) {
-        return prevServices.filter((id) => id !== serviceId);
-      } else {
-        return [...prevServices, serviceId];
-      }
-    });
   };
 
   useEffect(() => {
-    const handleOutsideClick = (event) => {
-      if (filterRef.current && !filterRef.current.contains(event.target)) {
-        setShowFilter(false);
-      }
-    };
-
-    const handleScroll = () => {
-      if (filterRef.current) {
-        const filterRect = filterRef.current.getBoundingClientRect();
-        if (filterRect.bottom <= 0) {
-          setShowFilter(false);
-        }
-      }
-    };
-
-    document.addEventListener("mousedown", handleOutsideClick);
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  useEffect(() => {
-    const fetchServices = async () => {
-      try {
-        const response = await fetch("http://127.0.0.1:8000/api/services");
-        const servicesData = await response.json();
-        setServices(servicesData);
-      } catch (error) {
-        console.error("Error fetching data from API:", error);
-      }
-    };
-
     const fetchCategories = async () => {
       try {
         const response = await fetch("http://127.0.0.1:8000/api/categories");
@@ -95,17 +38,7 @@ const Rentals = (props) => {
       }
     };
 
-    const fetchRoomService = async () => {
-      try {
-        const response = await fetch("http://127.0.0.1:8000/api/room-service");
-        const roomServiceData = await response.json();
-        setRoomServices(roomServiceData);
-      } catch (error) {
-        console.error("Error fetching data from API:", error);
-      }
-    };
-
-    Promise.all([fetchServices(), fetchCategories(), fetchRoomService()])
+    Promise.all([fetchCategories()])
       .then(() => {
         // Once all API calls are completed successfully, update the filteredData state
         setFilteredData(data);
@@ -131,42 +64,26 @@ const Rentals = (props) => {
 
   const applyFilters = () => {
     let filteredRooms = [...data];
-  
+
     // Apply category filter
-  if (selectedOptions !== "") {
-    filteredRooms = filteredRooms.filter(
-      (room) => room.category_id.toString() === selectedOptions
-    );
-  }
-  
+    if (selectedOptions !== "") {
+      filteredRooms = filteredRooms.filter(
+        (room) => room.category_id.toString() === selectedOptions
+      );
+    }
+
     // Apply room price filter
     if (roomPrice !== null) {
       filteredRooms = filteredRooms.filter((room) => room.price <= roomPrice);
     }
-  
-    // Apply room service filter
-    if (selectedServices.length > 0) {
-      filteredRooms = filteredRooms.filter((room) => {
-        if (room.roomServices && room.roomServices.length > 0) {
-          const roomServiceIds = room.roomServices.map(
-            (roomServiceData) => roomServiceData.service_id
-          );
-          return selectedServices.some((serviceId) =>
-            roomServiceIds.includes(serviceId)
-          );
-        }
-        return false;
-      });
-    }
-  
+
     setFilteredData(filteredRooms);
   };
-  
 
   useEffect(() => {
     // Call the applyFilters function whenever the filter options change
     applyFilters();
-  }, [selectedOptions, roomPrice, selectedServices]);
+  }, [selectedOptions, roomPrice]);
 
   const checkboxListStyle = {
     maxHeight: "70px",
@@ -175,49 +92,61 @@ const Rentals = (props) => {
 
   return (
     <div>
-      <button
+      <div
+        fullscreen
         style={{
+          backgroundImage:
+            "url('https://digital.ihg.com/is/image/ihg/ihgor-member-rate-web-offers-1440x720')",
+          backgroundSize: "100% auto",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
           position: "relative",
-          left: 400,
-          borderColor: "#ff5a60",
-          borderWidth: "2px",
-          borderStyle: "solid",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "600px",
+          alignItems: "center",
         }}
-        className="hidden lg:flex justify-center items-center relative rounded-full"
-        onClick={handleIconClick}
       >
         <div
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            width: "100%",
-          }}
-        >
-          <div className="flex justify-between relative w-full font-semibold text-gray-600">
-            <span style={{ marginRight: "9rem" }}>Room type</span>
-            <span style={{ marginRight: "9rem" }}>Service</span>
-            <span style={{ marginRight: "1rem" }}>Room price</span>
-          </div>
-          <div className="bg-[#ff5a60] p-2 rounded-full">
-            <FiSearch className="text-white w-full" />
-          </div>
-        </div>
-      </button>
-
-      {showFilter && (
-        <div
-          ref={filterRef}
-          style={{
+            color: "white",
             left: 0,
             right: 0,
-            width: 600,
-            height: 100,
             marginLeft: "auto",
             marginRight: "auto",
             display: "flex",
-            justifyContent: "space-between",
             alignItems: "center",
+            justifyContent: "center", // Thêm thuộc tính justifyContent với giá trị 'center'
+          }}
+        >
+          <h1
+            style={{
+              fontSize: "80px",
+              lineHeight: 1.2,
+              fontWeight: 700,
+              fontFamily: "",
+              textAlign: "center", // Căn giữa ngang dòng chữ
+              verticalAlign: "middle", // Căn giữa dọc dòng chữ
+            }}
+          >
+            Most Relaxing Place
+          </h1>
+        </div>
+
+        <div
+          style={{
+            left: 0,
+            right: 0,
+            width: 800,
+            height: 150,
+            marginLeft: "auto",
+            marginRight: "auto",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            position:"absolute",
+            top:450,
           }}
           className=" bg-white mt-2 w-[30rem] border border-gray-200 rounded-lg shadow-lg"
         >
@@ -232,9 +161,11 @@ const Rentals = (props) => {
             }}
             onClick={handleAllOption}
           >
-            All
+            Show All
           </button>
+          
           <label style={{ marginBottom: "5px" }}>
+            <h3>Room Type</h3>
             <select
               style={{ padding: 10 }}
               value={selectedOptions}
@@ -251,27 +182,8 @@ const Rentals = (props) => {
             </select>
           </label>
           <label>
-            <div style={checkboxListStyle}>
-              {services &&
-                services.map((service) => (
-                  <div
-                    key={service.id}
-                    className="flex items-center"
-                    style={{ padding: 10 }}
-                  >
-                    <input
-                      type="checkbox"
-                      value={service.id.toString()}
-                      checked={selectedServices.includes(service.id.toString())}
-                      onChange={handleServiceChange}
-                    />
-                    <span>{service.name}</span>
-                  </div>
-                ))}
-            </div>
-          </label>
-          <label>
             <div style={{ padding: 10 }}>
+              <h3>Room Price</h3>
               <input
                 type="range"
                 min="0"
@@ -287,7 +199,7 @@ const Rentals = (props) => {
             </div>
           </label>
         </div>
-      )}
+      </div>
 
       <div className="py-3 sm:py-5">
         <div
