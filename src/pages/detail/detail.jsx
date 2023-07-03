775ac43b03cd0569a3c8e68b2ca73ac0ff659c4c
thibackend <thi.a24technology@gmail.com>
2023-07-03 16:46:36 +0700

    have merge to move detal

--- src/pages/detail/detail.jsx
+++ src/pages/detail/detail.jsx
@@ -5,16 +5,16 @@ import axios from 'axios';
 // import { register } from 'swiper/element/bundle';
 // register Swiper custom elements
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { Link, NavLink, Navigate, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Card from 'react-bootstrap/Card';
 import { getRooms } from "../../services/home";
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import StarIcon from '@mui/icons-material/Star';
 import Checkout from "../checkout/Checkout";
-// import Services from "../services";
+import Services from "./services";
 
 function Detail() {
-  // register();
+  register();
   const { id } = useParams();
   const [data, setData] = useState(false);
   const [roomrelevant, setRoomrelevant] = useState(null);
@@ -216,7 +216,7 @@ function Detail() {
         <div className="services">
           <h1 className="title">SERVICES</h1>
           <ul className="service-item">
-            {/* <Services id={id} /> */}
+            <Services id={id} />
           </ul>
           <div className="service-item">
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,14 +5,8 @@ import AuthLayout from "../pages/auth/AuthLayout";
 import { Login, Register } from "../pages/auth";
 import Admin from "../pages/Admin/index";
 import AdminPage from '../pages/Admin/room_management/adminPage';
-import Rentalsnews from '../pages/home1/Rentalsnews';
 import User_manag from '../pages/Admin/User_management/user_manag';
 import Booking_mang from '../pages/Admin/booking_management/booking_ma';
-import About from '../pages/about/about';
-import Admin from "../pages/Admin";
-import Room_management from "../pages/Admin/room_management/room_manag";
-import Booking_mang from "../pages/Admin/booking_management/booking_ma";
-import User_manag from "../pages/Admin/User_management/user_manag";
 import Rentalsnews from "../pages/home1/Rentalsnews";
 import About from "../pages/about/about";
 import Detail from "../pages/detail/detail";
@@ -33,7 +27,6 @@ export const routes = () => [
       { path: "", element: <Rentalsnews /> },
       { path: "about", element: <About /> },
       // { path: "room", element: <Rooms /> },
-      { path: "about", element: <About/> },
       { path: "detail/:id", element: <Detail /> },
       // { path: "detailHotel/:id", element: <HotelDetail /> },
       // { path: "confirm", element: <ConfirmCheckout /> },
