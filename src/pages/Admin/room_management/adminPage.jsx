22c1f6ba1a6590ca7ed0f1ba8ad9cc04096b26f6
Tien-PM24 <115676176+Tien-PM24@users.noreply.github.com>
2023-07-03 09:21:09 +0700

    Fix and use only adminpage to CRUD

--- src/pages/Admin/room_management/adminPage.jsx
+++ src/pages/Admin/room_management/adminPage.jsx
@@ -1,43 +1,164 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import Loading from '../loading';
+import { Modal, Button } from 'react-bootstrap';
 
 function AdminPage() {
   const [loading, setLoading] = useState(true);
   const [rooms, setRooms] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentAction, setCurrentAction] = useState(null);
+  const [currentRoom, setCurrentRoom] = useState(null);
 
-  const openModal = (action) => {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [price, setPrice] = useState("");
+  const [category_id, setCategory_id] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [nameError, setNameError] = useState(false);
+  const [descError, setDescError] = useState(false);
+  const [priceError, setPriceError] = useState(false);
+  const [categoryError, setCategoryError] = useState(false);
+
+  const openModal = (action, room) => {
     setCurrentAction(action);
     setIsModalOpen(true);
+    setCurrentRoom(room);
+    if (action === 'edit') {
+      setName(room.name);
+      setDesc(room.desc);
+      setPrice(room.price);
+      setCategory_id(room.category_id);
+    }
   };
-  
+
   const closeModal = () => {
     setCurrentAction(null);
     setIsModalOpen(false);
+    setCurrentRoom(null);
+    setName('');
+    setDesc('');
+    setPrice('');
+    setCategory_id('');
   };
 
   useEffect(() => {
-    axios
-      .get('http://127.0.0.1:8000/api/room-and-images')
-      .then(res => {
-        console.log(res);
-        setRooms(res.data);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    fetchData();
   }, []);
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/room-and-images');
+      setRooms(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleDescChange = (e) => {
+    setDesc(e.target.value);
+  };
+
+  const handlePriceChange = (e) => {
+    setPrice(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setCategory_id(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setNameError(false);
+    setDescError(false);
+    setPriceError(false);
+    setCategoryError(false);
+
+    let hasError = false;
+
+    if (name.trim() === "") {
+      setNameError(true);
+      hasError = true;
+    }
+
+    if (desc.trim() === "") {
+      setDescError(true);
+      hasError = true;
+    }
+
+    if (price.trim() === "") {
+      setPriceError(true);
+      hasError = true;
+    }
+
+    if (currentAction === "create" || currentAction === "edit") {
+      if (category_id === "") {
+        setCategoryError(true);
+        hasError = true;
+      }
+    }
+
+    if (hasError) {
+      return;
+    }
+
+    try {
+      if (currentAction === 'create') {
+        await axios.post('http://127.0.0.1:8000/api/rooms', {
+          name,
+          desc,
+          price,
+          category_id,
+        });
+      } else if (currentAction === 'edit') {
+        const roomId = currentRoom.id;
+        await axios.put(`http://127.0.0.1:8000/api/rooms/${roomId}`, {
+          name,
+          desc,
+          price,
+          category_id,
+        });
+      }
+
+      closeModal();
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const roomId = currentRoom.id;
+      await axios.delete(`http://127.0.0.1:8000/api/rooms/${roomId}`);
+      closeModal();
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/categories");
+      setCategories(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (loading) {
-    return (
-      <div>
-        <Loading />
-      </div>
-    )
+    return <div>Loading...</div>;
   }
 
   return (
@@ -46,7 +167,7 @@ function AdminPage() {
         <div className="col-md-12">
           <div className="card">
             <div className="card-header">
-              <Link to={`create`} className="btn btn-primary float-end">Thêm phòng</Link>
+              <Button variant="primary" onClick={() => openModal('create', null)}>Thêm phòng mới</Button>
             </div>
             <div className="card-body">
               <table className="table table-striped">
@@ -56,7 +177,7 @@ function AdminPage() {
                     <th>Tên phòng</th>
                     <th>Giá</th>
                     <th>Mô tả</th>
-                    <th>Hình ảnh phòng</th>
+                    <th>Hình ảnh</th>
                     <th>Actions</th>
                   </tr>
                 </thead>
@@ -79,14 +200,14 @@ function AdminPage() {
                           ))}
                         </td>
                         <td className='action-button'>
-                          <Link to={`edit`} className="btn btn-success">Change</Link>
-                          <Link to={`delete`} className="btn btn-danger">Delete</Link>
+                          <Button variant="success" onClick={() => openModal('edit', room)}>Chỉnh sửa</Button>
+                          <Button variant="danger" onClick={() => openModal('delete', room)}>Xóa</Button>
                         </td>
                       </tr>
                     ))
                   ) : (
                     <tr>
-                      <td colSpan="6">No rooms available.</td>
+                      <td colSpan="5">Không có phòng nào.</td>
                     </tr>
                   )}
                 </tbody>
@@ -95,6 +216,88 @@ function AdminPage() {
           </div>
         </div>
       </div>
+      <Modal show={isModalOpen} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{currentAction === 'create' ? 'Thêm phòng' : currentAction === 'edit' ? 'Chỉnh sửa phòng' : 'Xóa phòng'}</Modal.Title>
+        </Modal.Header>
+        {currentAction !== 'delete' && (
+          <Modal.Body>
+            <form onSubmit={handleSubmit}>
+              <div className="md-3">
+                <label htmlFor="name">Tên phòng</label>
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+                {nameError && <p className="error-message">Vui lòng nhập tên phòng *</p>}
+              </div>
+              <div className="md-3">
+                <label htmlFor="desc">Mô tả</label>
+                <textarea
+                  name="desc"
+                  className="form-control"
+                  value={desc}
+                  onChange={handleDescChange}
+                  rows={4}
+                />
+                {descError && <p className="error-message">Vui lòng nhập mô tả *</p>}
+              </div>
+              <div className="md-3">
+                <label htmlFor="price">Giá phòng/đêm</label>
+                <input
+                  type="number"
+                  name="price"
+                  className="form-control"
+                  value={price}
+                  onChange={handlePriceChange}
+                />
+                {priceError && (
+                  <p className="error-message">Vui lòng nhập giá phòng *</p>
+                )}
+              </div>
+              {currentAction === 'create' || currentAction === 'edit' ? (
+                <div className="md-3">
+                  <label htmlFor="category_id">Loại phòng</label>
+                  <select
+                    name="category_id"
+                    className="form-control"
+                    value={category_id}
+                    onChange={handleCategoryChange}
+                  >
+                    <option value="">Chọn loại phòng</option>
+                    {categories.map((category) => (
+                      <option key={category.id} value={category.id}>
+                        {category.name}
+                      </option>
+                    ))}
+                  </select>
+                  {categoryError && <p className="error-message">Vui lòng chọn loại phòng *</p>}
+                </div>
+              ) : null}
+              <Button variant="primary" type="submit">
+                {currentAction === 'create' ? 'Thêm' : 'Lưu'}
+              </Button>
+              <Button variant="secondary" onClick={closeModal}>
+                Đóng
+              </Button>
+            </form>
+          </Modal.Body>
+        )}
+        {currentAction === 'delete' && (
+          <Modal.Body>
+            <p>Bạn có chắc chắn muốn xóa phòng này?</p>
+            <Button variant="danger" onClick={handleDelete}>
+              Xóa
+            </Button>
+            <Button variant="secondary" onClick={closeModal}>
+              Hủy
+            </Button>
+          </Modal.Body>
+        )}
+      </Modal>
     </div>
   );
 }
