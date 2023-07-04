b5897d1c3f1399b0f1e56a0990181c7a5b0ef298
thibackend <thi.a24technology@gmail.com>
2023-07-04 12:15:37 +0700

    add form

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,47 +1,64 @@
 import React, { useState, useEffect } from "react";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import '../../css/roomDetail.css';
-import { RommServices } from "../../services/home";
-const Checkout = ({ id }) => {
+const Checkout = (props) => {
     const [services, setServices] = useState(null);
+    const [credit, setCredit] = useState(false);
     const [selectedServices, setSelectedServices] = useState([]);
+
+    // hàm này kiểm tra người dùng click chọn bao nhiêu services.
     const handleCheckboxChange = (event) => {
         const value = event.target.value;
         const checked = event.target.checked;
-        // Nếu ô checkbox được chọn, thêm giá trị vào mảng
-        // Nếu ô checkbox bị bỏ chọn, loại bỏ giá trị khỏi mảng
         if (checked) {
             setSelectedServices([...selectedServices, value]);
         } else {
             setSelectedServices(selectedServices.filter((service) => service !== value));
         }
     };
-    const fetch = () => {
-        RommServices(id).then(
-            res => setServices(res)
-        )
+
+    // ----------------------------------------------------------------------------------------------
+
+
+
+
+
+    // hàm này dùng để kiểm tra thay đổi của credit khi mà người dùng click vào thì chuyển sang trạng thái true và hiện ô nhập số thẻ
+    const handleCreditCardChange = (event) => {
+        if (event.target.checked) {
+            setCredit(true);
+        } else {
+            setCredit(false);
+        }
     }
+    // ----------------------------------------------------------------------------------
+    // hàm này dùng để tạo validate form use Yup.
+
+
+
     useEffect(() => {
         if (!services) {
-            fetch();
+            setServices(props.services)
         }
-        console.log("services in checkout :", services);
+        console.log("in checkout services:", services);
     }, [services]);
+
     return (
         <div className="row">
             <div className="col-md-12">
-                <div className="card">
-                    <div className="card-header">
-                        <h2>$/night!</h2>
+                <div className="card shadow-2">
+                    <div style={{ background: '#ff5a60' }} className="card-header">
+                        <h2 >$4000 / night</h2>
                     </div>
                     <form>
                         <div className="card-body">
                             <div className="row">
-                                <div className="col-md-12 black-border">
+                                <div className="col-md-12">
                                     <div className="row">
                                         <div className="col-md-6">
                                             <div className="form-group mb-3">
                                                 <label>CHECK-IN</label>
-                                                {/* onChange={handleInput} value={checkoutInput.firstname}  */}
                                                 <input type="date" name="checkin_day" className="checkinday form-control  bg-light" />
                                                 {/* <small className="text-danger">{error.firstname}</small> */}
                                             </div>
@@ -56,100 +73,68 @@ const Checkout = ({ id }) => {
                                         </div>
                                     </div>
                                     <div className="row justify-content-center">
-                                        <div className="col-md-12 black-border">
+                                        <div className="col-md-12">
                                             {/* show services */}
-                                            <div className="row justify-content-between">
-                                                {/* {services ?
-                                                    services.services.map(
-                                                        (e, i) => ( */}
-                                                <div class="col-md-6 my-2">
-                                                    <label htmlFor="service">
-                                                        <input
-                                                            type="checkbox"
-                                                            name="services"
-                                                            // value={e.id}
-                                                            id="service"
-                                                        // onChange={handleCheckboxChange}
-                                                        />
-                                                        Serviec cham soc tre nhow
-                                                    </label>
-                                                </div>
-                                                <div class="col-md-6 my-2">
-                                                    <label htmlFor="service">
-                                                        <input
-                                                            type="checkbox"
-                                                            name="services"
-                                                            // value={e.id}
-                                                            id="service"
-                                                        // onChange={handleCheckboxChange}
-                                                        />
-                                                        Serviec cham soc tre nhow
-                                                    </label>
-                                                </div><div class="col-md-6 my-2">
-                                                    <label htmlFor="service">
-                                                        <input
-                                                            type="checkbox"
-                                                            name="services"
-                                                            // value={e.id}
-                                                            id="service"
-                                                        // onChange={handleCheckboxChange}
-                                                        />
-                                                        Serviec cham soc tre nhow
-                                                    </label>
-                                                </div><div class="col-md-6 my-2">
-                                                    <label htmlFor="service">
-                                                        <input
-                                                            type="checkbox"
-                                                            name="services"
-                                                            // value={e.id}
-                                                            id="service"
-                                                        // onChange={handleCheckboxChange}
-                                                        />
-                                                        Serviec cham soc tre nhow
-                                                    </label>
-                                                </div><div class="col-md-6 my-2">
-                                                    <label htmlFor="service">
-                                                        <input
-                                                            type="checkbox"
-                                                            name="services"
-                                                            // value={e.id}
-                                                            id="service"
-                                                        // onChange={handleCheckboxChange}
-                                                        />
-                                                        Serviec cham soc tre nhow
-                                                    </label>
-                                                </div>
-
-
-
-                                                {/* )
+                                            <div className="row justify-content-between b-black rounded">
+                                                {services ?
+                                                    services.map(
+                                                        (e, i) => (
+                                                            <div className="col-md-6 my-2" key={e.id}>
+                                                                <label htmlFor="service">
+                                                                    <input
+                                                                        type="checkbox"
+                                                                        name="services"
+                                                                        // value={e.id}
+                                                                        id="service"
+                                                                        onChange={handleCheckboxChange}
+                                                                    />
+                                                                    {e.name}
+                                                                </label>
+                                                            </div>
+                                                        )
                                                     )
-
-                                                    :
-                                                    ""} */}
-
+                                                    : ''
+                                                }
                                             </div>
-
                                             {/* show so luong nguoi */}
                                             <div className="col-md-12">
+                                                <div className="row align-items-center">
+                                                    <div className="col-md-6 border-bottom rounded">
+                                                        <label htmlFor="credit">
+                                                            <input type="checkbox" name="checkCredit" id="credit" placeholder="Enter amount people" onChange={handleCreditCardChange} />
+                                                            the tin dung
+                                                        </label>
+                                                    </div>
+                                                    {
+                                                        credit &&
+                                                        <div className="row my-2 shadow rounded">
+                                                            <div className="form-group">
+                                                                <label htmlFor="credit">Your credit card</label>
+                                                                <input type="number" className="form-control" name="credit" id="credit" placeholder=" Card number" />
+                                                            </div>
+                                                        </div>
+                                                    }
+                                                </div>
+                                            </div>
+                                            <div className="col-md-12 my-3">
                                                 <div className="row justify-content-between align-items-center">
                                                     <div className="col-md-6 border-bottom rounded">
-                                                        <input type="number" name="people" id="people" placeholder="Enter amount people" max={3} />
+                                                        <input type="number" className="border-none" name="people" id="people" placeholder="Enter amount people" />
                                                     </div>
                                                     <div className="col-md-6 border-bottom rounded">
                                                         <h6 >total: 3000$</h6>
                                                     </div>
                                                 </div>
-
                                             </div>
+
 
                                         </div>
                                     </div>
 
                                     <div className="col-lg-12">
-                                        <div className="form-group text-end">
+                                        <div className="form-group text-black">
                                             <button
-                                                type="submit" className="nutdat dir dir-ltr">BOOK</button>
+                                                type="submit" className="nutdat dir dir-ltr hover-zoom">BOOK</button>
                                         </div>
                                     </div>
                                 </div>
--- src/pages/detail/detail.jsx
+++ src/pages/detail/detail.jsx
@@ -10,25 +10,29 @@ import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import StarIcon from '@mui/icons-material/Star';
 import Checkout from "../checkout/Checkout";
 import Services from "./services";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 
 function Detail() {
   const { id } = useParams();
   const [data, setData] = useState(false);
-  const [roomrelevant, setRoomrelevant] = useState(null);
-  // const fetchRoom = async () => {
-  //   await axios.get(`http://localhost:8000/api/getOne-room-and-images/${id}`)
-  //     .then(
-  //       (res) => {
-  //         console.log("fist data of room detail:", res.data);
-  //         if (res.data) {
-  //           const object = res.data.find((e) => e);
-  //           setData(object);
-  //         }
-  //       }
-  //     )
-  //     .catch(errors => console.log(errors))
-  // }
+  // const [roomrelevant, setRoomrelevant] = useState(null);
+  // hàm này dùng để chúng ta lấy một room và tất cả các ảnh của room đó.
+  const fetchRoom = async () => {
+    await axios.get(`http://localhost:8000/api/getOne-room-and-images/${id}`)
+      .then(
+        (res) => {
+          console.log("fist data of room detail:", res.data);
+          if (res.data) {
+            const object = res.data.find((e) => e);
+            setData(object);
+          }
+        }
+      )
+      .catch(errors => console.log(errors))
+  }
   // const fetchAllRoom = () => {
   //   getRooms().then(
   //     res => {
@@ -50,29 +54,32 @@ function Detail() {
 
   // start 5      index<5 
   // index 1 
-  const Start = (star) => {
-    const arraystar = [];
-    for (let index = 0; index < 5; index++) {
-      if (star > index)
-        arraystar.push(...arraystar, <StarIcon />)
-      else {
-        arraystar.push(...arraystar, <StarOutlineIcon />)
-      }
-    }
-    return arraystar;
-  }
+  // const Start = (star) => {
+  //   const arraystar = [];
+  //   for (let index = 0; index < 5; index++) {
+  //     if (star > index)
+  //       arraystar.push(...arraystar, <StarIcon />)
+  //     else {
+  //       arraystar.push(...arraystar, <StarOutlineIcon />)
+  //     }
+  //   }
+  //   return arraystar;
+  // }
 
   // console.log("chieu dai ",data.image_path.length);
-  // useEffect(() => {
-  //   if (!data) {
-  //     fetchRoom();
-  //   }
-  //   if (data && !roomrelevant) {
-  //     fetchAllRoom();
+  useEffect(() => {
 
-  //   }
-  //   console.log("room relevant:", roomrelevant)
-  // }, [data, roomrelevant]);
+    if (!data) {
+      fetchRoom();
+    }
+    // if (data && !roomrelevant) {
+    //   fetchAllRoom();
+
+    // }
+    console.log("in detail page get services:", data.services)
+  }, [data]);
+
+
   return (
     <>
       <div className="container-fluid">
@@ -83,55 +90,82 @@ function Detail() {
         <div className="row">
           <div className="col-lg-8 col-sm-12">
             <div className="col-sm-12">
-              <img src="https://hc.com.vn/i/ecommerce/media/GS.009873_FEATURE_122334.jpg" alt="anh tuuongt truong" />
-              <div className="d-text">
+              <Slider
+                dots={true}
+                infinite={true}
+                speed={500}
+                slidesToShow={1}
+                slidesToScroll={1}
+              >
+                {
+                  data ?
+                    data.img_path || null ?
+                      data.img_path.map(e =>
+                      (
+                        <img key={e.id} src={e.img_path} alt='anh nen' />
+                      )
+                      )
+                      :
+                      <img src={'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg'} alt="anh" />
+                    : <h5>have no data</h5>
+                }
+              </Slider>
+              {/* <div>
+                   <img src={img_path} alt="" /> 
+                </div> */}
+              <div className="h-25 p-3 d-text">
                 <div className="d-sm-flex flex-row justify-content-start">
-                  <div className="black-border rounded  border-primary">5.0</div>
-                  <div className="black-border rounded  border-warning">Perfect</div>
+                  <div className="black-border rounded b-black">5.0</div>
+                  <div className="black-border rounded  border-warning  hover-shadow">Perfect</div>
+                  <div className="black-border rounded  border-primary">Top Value</div>
+                  <div className="black-border rounded  border-primary">Vip room</div>
                   <div className="black-border rounded  border-primary">Top Value</div>
                   <div className="d-flex flex-row align-items-center">
-                    <StarIcon />
-                    <StarIcon />
-                    <StarIcon />
-                    <StarIcon />
-                    <StarIcon />
+                    <StarIcon style={{ color: 'yellow' }} />
+                    <StarIcon style={{ color: 'yellow' }} />
+                    <StarIcon style={{ color: 'yellow' }} />
+                    <StarIcon style={{ color: 'yellow' }} />
+                    <StarIcon style={{ color: 'yellow' }} />
+
                   </div>
                 </div>
-                <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </h3>
+                <h3>{data.name} </h3>
               </div>
             </div>
           </div>
           <div className="col-md-4 col-sm-12">
             <div className="d-inline-sm-flex">
-              <Checkout id={id} />
+              {data && <Checkout id={id} services={data.services} />}
+
             </div>
           </div>
         </div>
-      </div>
+        <hr style={{ margin: "10px" }} />
 
-
-      <hr style={{ margin: "10px" }} />
-      <div className="service-price-typeroom">
-        <div className="services">
-          <h1 className="title">SERVICES</h1>
-          <ul className="service-item">
-            <Services id={id} />
-          </ul>
-          <div className="service-item">
+        <div className="row">
+          <div className="col-md-4 my-3">
+            <h5 className="title">Services</h5>
+            {
+              data && <Services services={data.services} />
+            }
 
           </div>
-        </div>
-        <div className="price">
-          <h1 className="title">PRICE</h1>
-          <h3>${data.price}  / Day</h3>
-        </div>
-        <div className="typeroom">
-          <h1 className="title">TYPE ROOM</h1>
-          <div className="h3">
-            {data ? data.category_name.name : <del> NONE</del>}
+          <div className="col-md-2 my-3">
+            <h5 className="title">Price</h5>
+            <h6>${data.price}</h6>
+
+          </div>
+          <div className="col-md-2 my-3">
+            <h5 className="title">Type room</h5>
+            <h6>{data && data.category_name && data.category_name.name}</h6>
+          </div>
+          <div className="col-md-4 my-3">
+            <h5 className="title">Comments</h5>
           </div>
         </div>
-
+        <div className="row">
+          
+        </div>
       </div>
     </>
   );
